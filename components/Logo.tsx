537a01e9fd092da95712bb6672942e5e0841bb3d
LeonardoGFlores <leogarf99@gmail.com>
2025-11-18 21:34:58 -0300

    Updated Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => (
-  <img 
-    src="C:\Users\l_gar\Desktop\Orienta Pecuária\logo orienta.jpg" 
-    alt="Orienta Logo" 
-    className={className} 
+  <img
+    src="/logo orienta.jpg"
+    alt="Orienta Logo"
+    className={className}
   />
 );
